e5b882c7a85832cc31ef8d84e50328ff1cdd4b8f
dtaylor <dtaylor@TrainingVM>
2021-09-11 12:05:20 -0500

    Make the test look a little better

--- basics/src/test/java/basics/SeleniumTests.java
+++ basics/src/test/java/basics/SeleniumTests.java
@@ -3,8 +3,12 @@ package basics;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTests {
 
@@ -21,5 +25,24 @@ public class SeleniumTests {
 
 		String currentUrl = driver.getCurrentUrl();
 		assertTrue("Did not navigate to the url " + url, currentUrl.equals(url));
+	}
+	
+	@Test
+	public void canShowExplicitWait() {
+		String chromeDriverPath = "C:\\\\Users\\dtaylor\\chromedriver.exe";
+		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, chromeDriverPath);
+		String baseUrl = "https://www.selenium.dev/";
+		String documentationPageUrl = baseUrl + "documentation/webdriver/";
+		long elementToBeClickableTimeoutSeconds = 5;
+		
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to(baseUrl);
+		WebElement button = driver.findElement(By.xpath("//h4[text()='Selenium WebDriver']/ancestor::div[@class='card h-100 border-0 bg-transparent']//a"));
+		WebDriverWait wait = new WebDriverWait(driver, elementToBeClickableTimeoutSeconds);
+		wait.until(ExpectedConditions.elementToBeClickable(button));
+		button.click();
+		String currentUrl = driver.getCurrentUrl();
+		
+		assertTrue("Did not navigate to the url " + documentationPageUrl, currentUrl.equals(documentationPageUrl));	
 	}
 }
