090c4f0eac7a26075ff3099cd6860c99f49459b4
dtaylor <dtaylor@TrainingVM>
2021-09-13 19:24:16 -0500

    Refactors basic tests by adding setup and cleanup

--- basics/src/test/java/basics/SeleniumTests.java
+++ basics/src/test/java/basics/SeleniumTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.time.Duration;
 import java.util.NoSuchElementException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,20 +19,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SeleniumTests {
 
 	final String WEB_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver";
+	private ChromeDriver driver;
+	private String baseUrl;
 
-	@Test
-	public void test() {
+	@Before
+	public void setup() {
 		String chromeDriverPath = "C:\\\\Users\\dtaylor\\chromedriver.exe";
 		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, chromeDriverPath);
-		String url = "https://www.selenium.dev/";
+		this.baseUrl = "https://www.selenium.dev/";		
 
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to(url);
-
-		String currentUrl = driver.getCurrentUrl();
-		assertTrue("Did not navigate to the url " + url, currentUrl.equals(url));
-
-		driver.quit();
+		this.driver = new ChromeDriver();
+		this.driver.navigate().to(this.baseUrl);
+	}
+	
+	@Test
+	public void test() {
+		String currentUrl = this.driver.getCurrentUrl();
+		
+		assertTrue("Did not navigate to the url " + this.baseUrl, currentUrl.equals(this.baseUrl));
 	}
 
 	@Test
@@ -90,5 +96,41 @@ public class SeleniumTests {
 		assertTrue("Did not navigate to the url " + documentationPageUrl, currentUrl.equals(documentationPageUrl));	
 
 		driver.quit();
+	}
+
+	@Test
+	public void canDemonstratePageObjectPattern() {
+		String chromeDriverPath = "C:\\\\Users\\dtaylor\\chromedriver.exe";
+		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, chromeDriverPath);
+		String baseUrl = "https://www.selenium.dev/";
+		
+		WebDriver driver = new ChromeDriver();
+
+		//1. Navigate to the Selenium Dev Homepage. 
+		driver.navigate().to(baseUrl);
+		//Expected: Page is shown.		
+	
+		//2. Click on Read More Button in Selenium WebDriver tile. 
+		WebElement button = driver.findElement(By.xpath("//h4[text()='Selenium WebDriver']/ancestor::div[@class='card h-100 border-0 bg-transparent']//a"));
+		button.click();
+		//Expected: User is taken to the Selenium WebDriver information page.
+		
+		//3. Click on the 'Understanding the Components' hyperlink. 
+		WebElement link = driver.findElement(By.linkText("Understanding the components"));
+		link.click();
+
+		//Expected: User is taken to the Understanding Components information page.
+		
+		//4. Click on the Selenium logo. 
+		WebElement homePageLink = driver.findElement(By.id("selenium_logo"));
+		homePageLink.click();
+		
+		//Expected: User is taken back to the Selenium Dev Homepage.
+	}
+
+
+	@After
+	public void cleanup() {
+		this.driver.quit();
 	}
 }
