4f89ae8a5c94076745ce9c4b283e87974c25d042
dtaylor <dtaylor@TrainingVM>
2021-09-17 16:59:30 -0500

    Adds Singleton

---
+++ basics/src/test/java/basics/designPatterns/DatabaseSingletonTests.java
@@ -0,0 +1,26 @@
+package basics.designPatterns;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+public class DatabaseSingletonTests {
+
+	@Test
+	public void verifySingletonReturnsSameInstance() {
+		DatabaseSingleton instanceOne = DatabaseSingleton.getInstance();
+		DatabaseSingleton instanceTwo = DatabaseSingleton.getInstance();
+		
+		assertTrue("Singleton is not returning same instance.", instanceOne.equals(instanceTwo));
+	}
+	
+	@Test
+	public void verifyCannotInstantiateSingleton() throws NoSuchMethodException, SecurityException {
+		Constructor<DatabaseSingleton> constructor = DatabaseSingleton.class.getDeclaredConstructor();
+		
+		assertTrue("Singleton constructor is not private.", Modifier.isPrivate(constructor.getModifiers()));
+	}
+}
