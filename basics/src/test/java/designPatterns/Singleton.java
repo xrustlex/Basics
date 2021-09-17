4b9b05f4602a73bc9d9b9fab35d1850118e30831
dtaylor <dtaylor@TrainingVM>
2021-09-17 18:06:29 -0500

    Rename tests

---
+++ basics/src/test/java/designPatterns/Singleton.java
@@ -0,0 +1,17 @@
+package designPatterns;
+
+public class Singleton {
+
+	private static Singleton singletonInstance = null;
+
+	private Singleton() {	
+	}
+	
+	public static Singleton getInstance() {
+		if(singletonInstance == null) {
+			singletonInstance = new Singleton();	
+		}
+
+		return singletonInstance;
+	}
+}
--- basics/src/test/java/designPatterns/SingletonTests.java
+++ basics/src/test/java/designPatterns/SingletonTests.java
@@ -7,19 +7,19 @@ import java.lang.reflect.Modifier;
 
 import org.junit.Test;
 
-public class DatabaseSingletonTests {
+public class SingletonTests {
 
 	@Test
 	public void verifySingletonReturnsSameInstance() {
-		DatabaseSingleton instanceOne = DatabaseSingleton.getInstance();
-		DatabaseSingleton instanceTwo = DatabaseSingleton.getInstance();
+		Singleton instanceOne = Singleton.getInstance();
+		Singleton instanceTwo = Singleton.getInstance();
 		
 		assertTrue("Singleton is not returning same instance.", instanceOne.equals(instanceTwo));
 	}
 	
 	@Test
 	public void verifyCannotInstantiateSingleton() throws NoSuchMethodException, SecurityException {
-		Constructor<DatabaseSingleton> constructor = DatabaseSingleton.class.getDeclaredConstructor();
+		Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor();
 		
 		assertTrue("Singleton constructor is not private.", Modifier.isPrivate(constructor.getModifiers()));
 	}
